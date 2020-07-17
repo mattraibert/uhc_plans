4688788eab2b0e48f04f24c66f0315cef0ce1123
Matt J Raibert <mattraibert@positiondev.com>
2020-07-17 17:31:55 -0400

    README

--- README.rb
+++ README.rb
@@ -2,6 +2,17 @@
 
 # run this file on the command line to generate a csv like this:
 # $ ./README.rb
+#
+# You can find a copy at:
+# https://docs.google.com/spreadsheets/d/1r9aB9lklK7FgjMkHknpUJs2BUL8T6OLdDKNQvg8vyWQ/edit?usp=sharing
+#
+# Some info for you:
+# You need access to the Choice Plus network to use insurance outside of tri-state area. I think only Freedom and
+# Liberty have Choice Plus access. This page has more info (click on "Medical - Oxford"):
+# https://www.uhc.com/employer/health-plans/new-york
+#
+# There is also a lot of helpful info here:
+# https://www.healthcare.gov/choose-a-plan/your-total-costs/
 
 require './plans'
 
